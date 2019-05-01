4d1ade6be604caf43d93e56ea9f0c68e0e840fc5
SenkiAlphonse <the.nagy.kriszta@gmail.com>
2019-05-01 17:30:29 +0200

    init

---
+++ src/main/java/com/delta/test_webapi/TestWebapiApplication.java
@@ -0,0 +1,13 @@
+package com.delta.test_webapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestWebapiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TestWebapiApplication.class, args);
+  }
+
+}
